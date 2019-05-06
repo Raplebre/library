b68416691cc89d0d246fbc1cdc9bcd466719ed52
Ruben Lebre <ruben.lebre@gmail.com>
2019-05-06 17:11:42 +0100

    New commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 const express = require('express')
-const port = process.env.port
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        dialect: 'mysql',
+        host: process.env.DB_HOST,
+        port: process.env.PORT
+    }
+)
+
+const app = express()
+app.use(express.json())
+
+console.log(sequelize.config)
